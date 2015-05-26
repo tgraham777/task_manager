22e883cdd7c3cfbe0abb1e103eb1430f0846c21e
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-05-26 09:37:07 -0600

    task_test updated

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,9 +4,9 @@ class TaskTest < Minitest::Test
   # inherit from ModelTest because it is in test_helper and already inherits
   # everything we need from MiniTest
   def test_assigns_attributes_correctly
-    task = Task.new({ "title"       => "a title",
-                      "description" => "a description",
-                      "id"          => 1 })
+    task = Task.new({ :title       => "a title",
+                      :description => "a description",
+                      :id          => 1 })
     assert_equal "a title", task.title
     assert_equal "a description", task.description
     assert_equal 1, task.id
